a4804f766ffb5e2bed43c96e80544c793a3867a6
Gary Sorohan <148573610+Soro82@users.noreply.github.com>
2024-02-13 11:49:23 +0000

    add function to get the stock values

--- run.py
+++ run.py
@@ -122,6 +122,24 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
-print("Welcome to Love Sandwiches Data Automation.")
-main()
+print("Welcome to Love Sandwiches data automation.\n")
+stock_data = main()
+
+
+def get_stock_values(data):
+    """
+    Builds a dictionary, where the keys are the sandwich headings 
+    pulled from the spreadsheet, and the values are the calculated 
+    stock data.
+    """
+    headings = SHEET.worksheet('stock').row_values(1)
+    stock_dict = {}
+    for heading, stock in  zip(headings, data):
+        stock_dict[heading] = stock
+    
+    return stock_dict
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
